6a3758a2113088336acceb868013deae5dbc9f47
8BitRobot <ninjastrike454@gmail.com>
2025-08-15 09:21:22 -0700

    commit

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -3,93 +3,32 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
+    World.setDelay(10); 
 
+    
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
-    Robot r = new Robot(2,1,East,30);
+    Robot r = new Robot(1,5,North,30);
 	
     // examples of commands you can invoke on a Robot
-    r.putBeeper();
-    r.move();// move one step in the direction it is facing
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.move();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.move();
-    r.move();
-    r.move();
-    r.turnLeft();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.turnLeft();
-    r.turnLeft();
-    r.turnLeft();
-    r.move();
-    r.putBeeper();
-    r.move();
-    r.putBeeper();
-    r.move();
-	r.putBeeper();
+    
 
-
+    for (int i = 0; i < 4; i++) {
+      r.putBeeper();
+      for (int u=0;u<4;u++) {
+        r.move();
+        r.turnLeft();
+        r.move();
+        r.putBeeper();
+        turnRight(r);
+      }
+      turnRight(r);
+    }
+    
+  }
+  public static void turnRight (Robot robot) {
+    for (int i = 0; i < 3; i++) {
+      robot.turnLeft();
+    }
   }
 }
